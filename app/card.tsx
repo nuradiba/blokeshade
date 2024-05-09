25777e5bff5626f1a150aab045fde9bf47b86913
Adiba <nuradiba@users.noreply.github.com>
2024-05-09 11:14:44 +0800

    Change card color

--- app/card.tsx
+++ app/card.tsx
@@ -4,13 +4,14 @@ import styles from './style.module.scss';
 import { useTransform, motion, useScroll } from 'framer-motion';
 import { useRef } from 'react';
 
-const Card = ({i, title, description, src, date, color, progress, range, targetScale}: {
+const Card = ({i, title, description, src, date, color, text, progress, range, targetScale}: {
     i: any;
     title: any;
     description: any;
     src: any;
     date: any;
     color: any;
+    text: any;
     progress: any;
     range: any;
     targetScale: any;}) => {
@@ -30,12 +31,12 @@ const Card = ({i, title, description, src, date, color, progress, range, targetS
         style={{backgroundColor: color, scale, top:`calc(-5vh + ${i * 25}px)`}} 
         className={'h-[700px] md:h-[500px] '+styles.card}
       >
-        <h2 className='font-semibold'>{title}</h2>
+        <h2 className={'font-semibold '+text}>{title}</h2>
         <div className={'flex-col md:flex-row mt-0 md:mt-10 '+styles.body}>
-          <div className={'w-full md:w-5/12 '+styles.description}>
+          <div className={'w-full md:w-5/12 '+styles.description+' '+text}>
             <p>{description}</p>
             <span>
-              <small className='italic text-gray-100 '>{date}</small>
+              <small className={'italic text-gray-100 '+text}>{date}</small>
             </span>
           </div>
 
