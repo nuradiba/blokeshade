10d8a8a631bfcb2c32edaf2eea730efc253f0e46
Adiba <nuradiba@users.noreply.github.com>
2026-04-22 12:08:57 +0800

    Remove zoom from about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,9 @@
 "use client"
-import Zoom from '../components/zoom'
 
 export default function About() {
 
     return (
         <section className='pt-24'>
-            <Zoom />
         </section>
     );
 }
